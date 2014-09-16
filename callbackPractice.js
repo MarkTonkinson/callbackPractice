5b2a07b1a571754258cab924088eb05cef805d17
Mark Tonkinson <DHTMaster@gmail.com>
2014-09-16 13:15:59 -0600

    callbacks completed

--- callbackPractice.js
+++ callbackPractice.js
@@ -4,33 +4,101 @@
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
-first(names, function(firstName){
-  console.log('The first name in names is ', firstName)
-});
+var logger1 = function(nameFromArray){
+  console.log('The first name in names is ', nameFromArray)
+};
 
-last(names, function(lastName){
-  console.log('The last name in names is ', lastName);
-});
+var firstName = function(arr, cb){
+  var name1 =  arr[0];
+  cb(name1);
+}
+firstName(names, logger1);
 
-multiply(num1, num2, function(answer){
+var last = function(lastName){
+  console.log('The last name in names is ' + lastName);
+};
+
+var lastName = function(arr, cb){
+  var nameLast = arr[arr.length - 1];
+  cb(nameLast);
+}
+
+lastName(names, last);
+
+var mathAnswer = function(answer){
   console.log('The answer is ', answer);
-})
+}
 
-contains(names, 'Colt', function(yes){
-  if(yes === true){
-    'Colt is in the array';
-  } else {
-    'Colt is not in the list';
+var doMath = function(num1, num2, cb){
+  var multiplyAns = num1 * num2;
+  cb(multiplyAns);
+}
+
+doMath(3, 4, mathAnswer);
+
+//contains(names, 'Colt', function(yes){
+//  if(yes === true){
+//    'Colt is in the array';
+//  } else {
+//    'Colt is not in the list';
+//  }
+//});
+
+
+var doesItContain = function(result){
+  console.log(result);
+}
+
+var contains = function(arr, nameTesting, cb){
+  for(var i = 0; i < arr.length; i++){
+    if (arr[i] === nameTesting){
+      var yes = true;
+    }
   }
-});
+  var ifResult;
+  if(yes === true){
+    ifResult = nameTesting + ' is in the array';
+  } else {
+    ifResult = nameTesting + ' is not in the list';
+  }
+  cb(ifResult);
+};
 
-uniq(names, function(uniqArr){
-  console.log('The new names array with all the duplicate items removed is ', uniqArr);
-});
+contains(names, 'Colt', doesItContain);
 
-each(names, function(item, indice){
-  console.log('The item in the ' + indice + 'position is ' + item)
-});
+//uniq(names, function(uniqArr){
+
+var fixedArrayLogger = function(uniqArr){
+  console.log('The new names array with all the duplicate items removed is ' + uniqArr);
+};
+
+var uniq = function(arr, cb) {
+  for(var i = 0; i < arr.length; i++) {
+    for(var j = i + 1; j < arr.length; j++) {
+       if(arr[j] === arr[i]){
+         arr.splice(j, 1);
+         j--;
+       }
+    };
+  };   
+  cb(arr);
+};
+
+uniq(names, fixedArrayLogger);
+
+////////////////////////////////////////////
+var each = function(item, indice){
+  console.log('The item in the ' , indice , 'position is ' , item)
+}
+
+var objLoop = function(arr) {
+  for (var i = 0; i < arr.length; i++){
+    cb(i, arr[i]);
+  }
+}
+//each(names, function(item, indice){
+  
+
 
 var users = [
   {
@@ -53,11 +121,22 @@ var users = [
   },
 ];
 
-getUserById('16t', function(user){
+//getUserById('16t', function(user){
+//  console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
+//});
+
+var getUserById = function(user) {
   console.log('The user with the id 16t has the email of ' + user.email + 'the name of ' + user.name + ' and the address of ' + user.address); 
-});
+}
 
-
+var searchUser = function(arr, userId, cb) {
+  for(var i = 0; i < arr.length; i++){
+    if (arr[i].id === userId){
+      cb(arr[i]);
+    }
+  }
+}
+searchUser(users, '16t', getUserById);
 
 
 
